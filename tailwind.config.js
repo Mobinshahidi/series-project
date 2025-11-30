b1ef712131499c8e819db5f4ef913598288666bf
mobinshahidi <mowbinsh@gmail.com>
2025-11-30 23:11:04 +0330

    c

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // we toggle html.dark from JS
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
